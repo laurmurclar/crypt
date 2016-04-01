5be1cff9454dd9533f0889dfd8f78518f5d51d88
Laura Murphy-Clarkin <laurmurclar@gmail.com>
2016-04-01 17:15:29 +0100

    add user

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :omniauthable,
+  #  :recoverable, :rememberable, :trackable
+  devise :database_authenticatable, :registerable, :validatable
+
+end
