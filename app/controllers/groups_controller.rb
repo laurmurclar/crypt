56cbd240e2f3bbf084f30e2cea73a079394542f3
Laura Murphy-Clarkin <laurmurclar@gmail.com>
2016-04-03 21:27:01 +0100

    add group controller with new and create actions.

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,23 @@
+class GroupsController < ApplicationController
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+
+    if @group.save
+      @group.members.create(user: current_user, role: 'owner')
+      redirect_to root_path
+    else
+      flash[:danger] = "Invalid name"
+    end
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name)
+  end
+
+end
