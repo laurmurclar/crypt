7f1d36bd1f3dea497ad6544c377e70923f5ea6cd
Laura Murphy-Clarkin <laurmurclar@gmail.com>
2016-04-03 22:06:07 +0100

    validate presence of group name

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,8 @@ class Group < ActiveRecord::Base
   has_many :members
   has_many :users, through: :members, dependent: :destroy
 
+  validates :name, presence: true
+
   def owner
     self.members.where(role: 'owner').first
   end
