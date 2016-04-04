a6bd22f1f709bbf7cdcccb4a699bcd4f907e11b1
Laura Murphy-Clarkin <laurmurclar@gmail.com>
2016-04-04 20:05:58 +0100

    move file encryption to its own method

--- app/controllers/edocs_controller.rb
+++ app/controllers/edocs_controller.rb
@@ -17,33 +17,9 @@ class EdocsController < ApplicationController
       flash[:success] = "File added"
 
       # encrypt
-      cipher = OpenSSL::Cipher.new('aes-256-cbc')
-      cipher.encrypt
-      key = cipher.random_key
-      iv = cipher.random_iv
-      @edoc.iv = iv.unpack('H*').join
-
-      buf = ""
-      File.open("file.bin", "wb") do |outf|
-        File.open(@edoc.doc.path, "rb") do |inf|
-          while inf.read(4096, buf)
-            outf << cipher.update(buf)
-          end
-          outf << cipher.final
-        end
-      end
-
-      File.open("file.bin", "rb") do |input|
-        File.open(@edoc.doc.path, "wb") do |output|
-          while buff = input.read(4096)
-            output.write(buff)
-          end
-        end
-      end
-      File.delete("file.bin")
-
+      encrypt_doc
       @edoc.save
-      
+
       redirect_to edocs_path(group_id: group)
     else
       flash[:success] = "Invalid file"
@@ -60,6 +36,34 @@ private
 
   def edoc_params
     params.require(:edoc).permit(:name, :doc)
+  end
+
+  def encrypt_doc
+    cipher = OpenSSL::Cipher.new('aes-256-cbc')
+    cipher.encrypt
+    key = cipher.random_key
+    iv = cipher.random_iv
+    @edoc.iv = iv.unpack('H*').join
+
+    buf = ""
+    File.open("file.bin", "wb") do |outf|
+      File.open(@edoc.doc.path, "rb") do |inf|
+        while inf.read(4096, buf)
+          outf << cipher.update(buf)
+        end
+        outf << cipher.final
+      end
+    end
+
+    File.open("file.bin", "rb") do |input|
+      File.open(@edoc.doc.path, "wb") do |output|
+        while buff = input.read(4096)
+          output.write(buff)
+        end
+      end
+    end
+
+    File.delete("file.bin")
   end
 
 end
