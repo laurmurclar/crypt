14ac18a33bd327f4f045d2d323fd3cdfa40c72e7
Laura Murphy-Clarkin <laurmurclar@gmail.com>
2016-04-04 14:03:01 +0100

    rename member routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   root to: "user#home"
 
   resources :groups
-  get '/groups/:id/new_member', to: 'groups#new_member', as: 'new_member'
-  post '/groups/:id/create_member', to: 'groups#create_member', as: 'create_member'
+  get '/groups/:id/member', to: 'groups#new_member', as: 'new_member'
+  post '/groups/:id/member', to: 'groups#create_member', as: 'create_member'
   get '/groups/:id/members', to: 'groups#members', as: 'members'
 
 end
